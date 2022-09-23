3ecf452b7bb0f323be270ddb5ae7949cf1cde5ad
Paul Willis <paul.willis@mkodo.com>
2022-09-23 19:21:08 +0100

    Buggy and untested initial state

--- MVVMRefactoringKata/MVVMRefactoringKata/ViewController.swift
+++ MVVMRefactoringKata/MVVMRefactoringKata/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var pressMeButton: UIButton!
+    @IBOutlet weak var countValueLabel: UILabel!
+    
+    private var countValue: Int = 0
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.countValueLabel.text = String(self.countValue)
     }
 
-
+    @IBAction func pressMeButtonTapped(_ sender: UIButton) {
+        self.countValue += 1
+        
+        if self.countValue == 10 {
+            self.mainView.backgroundColor = .green
+        }
+    }
 }
-
